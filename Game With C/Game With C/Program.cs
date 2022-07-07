987a79f38eeca11f95c2002752dd1a7d19b1b7d7
CRACKEDSPY <54416690+CRACKEDSPY@users.noreply.github.com>
2022-07-07 19:11:07 +0300

    Console Game with C#

---
+++ Game With C/Game With C/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_With_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Prediction = 0;
+            int NumberProduced = 0;
+            int Counter = 0;
+
+            Console.Write("Enter the Highest Value for the Guessing Game : ");
+            int KullanıcıGelenDeger = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            NumberProduced = rnd.Next(1, KullanıcıGelenDeger);
+
+            do
+            {
+                Counter++;
+                Console.WriteLine("{0} Estimate the Number Produced", Counter);
+                Prediction = int.Parse(Console.ReadLine());
+
+            } while (Prediction != NumberProduced);
+
+            Console.WriteLine("{0} You Tried Times You Guessed The Number.", Counter);
+
+            Console.ReadLine();
+        }
+    }
+}
